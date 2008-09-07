4aaded4d2e1d74d324c020aa86c38d046fb7087d
jasmine.aura@yahoo.com <jasmine.aura@yahoo.com@eea01fdd-3c56-0410-a947-61471c3eca9e>
2008-09-07 07:13:14 +0000

    Updated header of probe.php to comply with phpBB3 standard file headers

--- trunk/probe.php
+++ trunk/probe.php
@@ -1,21 +1,17 @@
 <?php
-/***************************************************************************
- *                                probe.php
- *                            -------------------
- *   begin                : Sunday, Sept 7, 2008
- *   copyright          : (C) jasmineaura
- *   copyright          : (C) MMVI TerraFrost
- *
- ***************************************************************************/
+/**
+*
+* @author TerraFrost < terrafrost@phpbb.com >
+* @author jasmineaura < jasmine.aura@yahoo.com >
+*
+* @package phpBB3
+* @version $Id: probe.php 6 2008-09-08 07:12:00GMT $
+* @copyright (c) 2006 TerraFrost
+* @copyright (c) 2008 jasmineaura
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License 
+*
+*/
 
-/***************************************************************************
- *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
- *
- ***************************************************************************/
 
 /**
 * @ignore
